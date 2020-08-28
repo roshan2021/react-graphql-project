6d4051ea40565985e0f835cb243c72a07eb0b038
Roshan Jacob <roshan.jacob2021@gmail.com>
2020-08-28 17:51:18 -0500

    styling components

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,16 +1,31 @@
 import React, { Component } from 'react'
 import Header from '../components/Header';
 import Meta from '../components/Meta';
+import styled from 'styled-components';
+
+const StyledPage = styled.div`
+    background: white;
+    color: black;
+`;
+
+const Inner = styled.div`
+    max-width: 1000px;
+    margin: 0 auto;
+    padding: 2rem;
+    background: red;
+`;
 
 class Page extends Component {
     render() {
         return (
-            <div>
+            <StyledPage>
                 <Meta />
                 <Header />
+                <Inner>
                 {this.props.children}
-            </div>
-        )
+                </Inner>
+            </StyledPage>
+        );
     }
 }
 

e4c9a43a81249ae3a20e98e3e359e68ee6f0e516
Roshan Jacob <roshan.jacob2021@gmail.com>
2020-09-04 17:49:54 -0500

    StyledHeader for Nav links, search and cart

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -21,22 +21,41 @@ const Logo = styled.h1`
     }
 `;
 
+const StyledHeader = styled.header`
+    .bar {
+        border-bottom: 10px solid ${props => props.theme.black};
+        display: grid;
+        grid-template-columns: auto 1fr;
+        justify-content: space-between;
+        align-items: stretch;
+        @media (max-width: 1300px){
+            grid-template-columns: 1fr;
+            justify-content: center;
+        }
+    }
+    .sub-bar{
+        display: grid;
+        grid-template-columns: 1fr auto;
+        border-bottom: 10px solid ${props => props.theme.lightgrey};
+    }
+`;
+
 
 const Header = () => (
-    <div>
-    <div className="bar">
-        <Logo>
-            <Link href="/">
-            <a>Sick Fits</a>
-            </Link>
-        </Logo>
-        <Nav />
-    </div>
-     <div className="sub-bar">
-         <p>Search</p>
-     </div>
-     <div>Cart</div>
-    </div>
+    <StyledHeader>
+        <div className="bar">
+            <Logo>
+                <Link href="/">
+                    <a>Sick Fits</a>
+                </Link>
+            </Logo>
+            <Nav />
+        </div>
+        <div className="sub-bar">
+            <p>Search</p>
+        </div>
+        <div>Cart</div>
+    </StyledHeader>
 );
 
 export default Header;

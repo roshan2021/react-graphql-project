93583cc3ba44a148fac42ca9a2641f972165dc91
Roshan Jacob <roshan.jacob2021@gmail.com>
2020-09-04 16:16:04 -0500

    homepage Link added

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,23 +1,33 @@
 import React, { Component } from 'react'
 import Header from '../components/Header';
 import Meta from '../components/Meta';
-import styled from 'styled-components';
+import styled, { ThemeProvider, injectGlobal } from 'styled-components';
+
+const theme = {
+    red: '#FF0000',
+    black: '#393939',
+    grey: '#3A3A3A',
+    lightgrey: '#E1E1E1',
+    offWhite: 'EDEDED',
+    maxWidth: '1000px',
+    bs: '0 12px 24px 0 rgba(0, 0, 0, 0.09)',
+};
 
 const StyledPage = styled.div`
     background: white;
-    color: black;
+    color: ${props => props.theme.black};
 `;
 
 const Inner = styled.div`
-    max-width: 1000px;
+    max-width: ${props => props.theme.maxWidth};
     margin: 0 auto;
     padding: 2rem;
-    background: red;
 `;
 
 class Page extends Component {
     render() {
         return (
+            <ThemeProvider theme={theme}>
             <StyledPage>
                 <Meta />
                 <Header />
@@ -25,6 +35,7 @@ class Page extends Component {
                 {this.props.children}
                 </Inner>
             </StyledPage>
+            </ThemeProvider>
         );
     }
 }

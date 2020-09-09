f23669c766e8e59e08b1229146d4cd317dfb3b08
Roshan Jacob <roshan.jacob2021@gmail.com>
2020-09-09 18:27:54 -0500

    made corrections to _app.js, added items.js and fixed Nav.js

--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -4,10 +4,10 @@ import { ApolloProvider } from 'react-apollo';
 import withData from '../lib/withData';
 
 class MyApp extends App {
-    static getInitialProps({ Component, ctx }) {
+    static async getInitialProps({ Component, ctx }) {
         let pageProps = {};
         if(Component.getInitialProps) {
-            pageProps = await    Component.getInitialProps(ctx);
+            pageProps = await Component.getInitialProps(ctx);
         }
         //this exposes the query to the user
         pageProps.query = ctx.query;
